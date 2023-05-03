ed866dbe64e89e51ed970dac0edf1dcf8d683982
Patricksmille12 <hakudoshinu151515@gmail.com>
2023-05-03 12:09:40 -0300

    feat: register page style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -13,13 +13,6 @@ const Login = () => {
           <h2 className="text-4x1 text-white font-bold text-center">
             Faça seu login
           </h2>
-          <div className="flex flex-col text-gray-400 py-2">
-            <label>Nome</label>
-            <input
-              className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
-              type="text"
-            />
-          </div>
 
           <div className="flex flex-col text-gray-400 py-2">
             <label>Email</label>
@@ -40,7 +33,7 @@ const Login = () => {
             Fazer o login
           </button>
           <p className="text-4x1 text-white font-bold text-center">ou</p>
-          <button className="w-full my-5 py-2 bg-teal-500 text-white font-semibold rounded-lg">
+          <button className="w-full my-5 py-2 bg-red-500 text-white font-semibold rounded-lg">
             Cadastre-se
           </button>
         </form>
