5bdc35ee2254d5ff0f03fecb08080309e051c814
Leonardo <leodaleth@gmail.com>
2023-05-04 17:33:09 -0300

    chore: criação de logo Home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
-import { StyledHeader } from './styled';
+import { Link } from "react-router-dom";
+import { StyledHeader } from "./styled";
+import Logo from "../../assets/Images/logoRacapajole-removebg-preview.png";
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <figure>
-        {' '}
-        <img src="" alt="Logo" />
-      </figure>
-      <div className="buttons__box">
-        <Link to={'#'}>Login</Link>
-        <Link to={'#'}>Cadastro</Link>
+      <div className="container__Header">
+        <figure>
+          {" "}
+          <img src={Logo} alt="Logo" />
+        </figure>
+        <div className="buttons__box">
+          <div className="button__Login">
+            <Link to={"#"}>Login</Link>
+          </div>
+          <div className="button__Cadastro">
+            <Link to={"#"}>Cadastro</Link>
+          </div>
+        </div>
       </div>
     </StyledHeader>
   );
---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+
+.container__Home{
+  display: flex;
+}
+
+
+`
+
+
