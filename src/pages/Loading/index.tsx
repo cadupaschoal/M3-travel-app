550eebaad1bfbadd3f3e26b6cc2083640b4d2f74
cadupaschoal <carloseduardopaschoal@hotmial.com>
2023-05-05 22:05:22 -0300

    feature: loading and not found pages

---
+++ src/pages/Loading/index.tsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <div className=" h-screen flex items-center justify-center bg-gray-700">
+      <div className="place-self-center h-12 w-12 border-4 border-l-gray-200 border-r-gray-200 border-b-gray-200 border-t-blue-500 animate-spin ease-linear rounded-full"></div>
+    </div>
+  );
+};
+
+export default LoadingPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,23 +3,22 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import { ProtectedRoutes } from './ProtectedRoutes';
-import HomePage from "./pages/Home"
-
-
-
+import HomePage from './pages/Home';
+import NotFound from './pages/NotFound';
 const Router = () => {
-    return(
-        <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            {/* <Route path="/" element={<Login />}></Route> */}
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-            <Route path="/dashboard" element={<ProtectedRoutes/>}>
-                <Route  index element={<Dashboard />}></Route>
-            </Route>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
 
+      {/* <Route path="/" element={<Login />}></Route> */}
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/dashboard" element={<ProtectedRoutes />}>
+        <Route index element={<Dashboard />}></Route>
+      </Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
 };
 
 export default Router;
