ddf239c7b3ff02128f15fdce95bdd42537443c18
JoãoFigueiredo <jp5398@hotmail.com>
2023-05-08 06:31:02 -0300

    fix: font-family

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -7,6 +7,7 @@ export const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   list-style: none;
   text-decoration: none;
+  font-style: 'Inter', sans-serif;
 }
 
 *::-webkit-scrollbar {
